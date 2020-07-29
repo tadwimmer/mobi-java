18fef7032806bd03f08e9d004aa4a2973e818c91
Tad Wimmer <tad.wimmer@utah.edu>
2020-07-29 16:53:58 -0600

    Added test case for PDBReader, tested with a random .mobi file, refactored to reduce complexity.

--- src/main/java/hu/webhejj/pdb/PDBReader.java
+++ src/main/java/hu/webhejj/pdb/PDBReader.java
@@ -10,6 +10,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 public class PDBReader {
@@ -18,12 +19,11 @@ public class PDBReader {
 
 	public PalmDataBase read(File file) throws IOException {
 		
-		Charset charset = Charset.forName("ASCII");
+		Charset charset = StandardCharsets.US_ASCII;
 		
-		DataInputStream dis = null;
-		try {
-			dis = new DataInputStream(new FileInputStream(file));
-			
+
+		try(DataInputStream dis = new DataInputStream(new FileInputStream(file))) {
+
 			PalmDataBase pdb = new PalmDataBase();
 			pdb.setName(ByteIO.readNullTerminatedString(dis, charset));
 			for(int i = 31; i > pdb.getName().length(); i--) {
@@ -42,7 +42,7 @@ public class PDBReader {
 			pdb.setCreator(ByteIO.readFixedLengthString(dis, 4, charset));
 			pdb.setUniqueIdSeed(dis.readLong());
 			
-			int entryCount = dis.readUnsignedShort();
+
 			
 			if(appInfoOffset != 0) {
 				throw new IllegalArgumentException("Can't handle app info yet, found offset " + appInfoOffset);
@@ -50,58 +50,53 @@ public class PDBReader {
 			if(sortInfoOffset != 0) {
 				throw new IllegalArgumentException("Can't handle app info yet, found offset " + sortInfoOffset);
 			}
-			
-			int[] offsets = new int[entryCount];
-			for(int i = 0; i < entryCount; i++) {
-				offsets[i] = dis.readInt();
-				PalmRecord palmRecord = new PalmRecord();
-				palmRecord.setAttributes(dis.readUnsignedByte());
-				palmRecord.setId((dis.readUnsignedByte() << 16) | dis.readUnsignedShort());
-				pdb.getPalmRecords().add(palmRecord);
-				// System.out.format("Offset %5d - %8d - %8x\n", i, offsets[i], offsets[i]);
 
-			}
-			
+			int entryCount = dis.readUnsignedShort();
+			int[] offsets = addPalmRecords(dis, pdb, entryCount);
+
 			// skip to first record
 			dis.skipBytes(offsets[0] - 78 - (8 * entryCount));
-			
-			
 
-			//MobiRecord mobiRecord = readMobiRecord(dis, offsets[1] - offsets[0]);
-			//StringBuilder textBuf = new StringBuilder(mobiRecord.getTextLenght());
-			int i = 0;
-			for(PalmRecord palmRecord: pdb.getPalmRecords()) {
-				
-				if(i < offsets.length -1) {
-					int length = offsets[i + 1] - offsets[i];
-					palmRecord.setData(new byte[length]);
-					dis.readFully(palmRecord.getData());
-					
-				} else {
-					byte[] buf = new byte[4096];
-					ByteArrayOutputStream baos = new ByteArrayOutputStream(dis.available());
-					int count = 0;
-					while((count = dis.read(buf)) >= 0) {
-						baos.write(buf, 0, count);
-					}
-					palmRecord.setData(baos.toByteArray());
-				}
-				i++;
-			}
-			
+			setDataIntoPalmRecords(dis, pdb, offsets);
 			return pdb;
-			
-		} finally {
-			if(dis != null) {
-				try {
-					dis.close();
-				} catch (IOException e) {
-					// ignore
-				}
-			}
 		}
 	}
-	
+
+	private void setDataIntoPalmRecords(DataInputStream dis, PalmDataBase pdb, int[] offsets) throws IOException {
+		int i = 0;
+		for(PalmRecord palmRecord: pdb.getPalmRecords()) {
+
+			if(i < offsets.length -1) {
+				int length = offsets[i + 1] - offsets[i];
+				palmRecord.setData(new byte[length]);
+				dis.readFully(palmRecord.getData());
+
+			} else {
+				byte[] buf = new byte[4096];
+				ByteArrayOutputStream baos = new ByteArrayOutputStream(dis.available());
+				int count;
+				while((count = dis.read(buf)) >= 0) {
+					baos.write(buf, 0, count);
+				}
+				palmRecord.setData(baos.toByteArray());
+			}
+			i++;
+		}
+	}
+
+	private int[] addPalmRecords(DataInputStream dis, PalmDataBase pdb, int entryCount) throws IOException {
+		int[] offsets = new int[entryCount];
+		for(int i = 0; i < entryCount; i++) {
+			offsets[i] = dis.readInt();
+			PalmRecord palmRecord = new PalmRecord();
+			palmRecord.setAttributes(dis.readUnsignedByte());
+			palmRecord.setId((dis.readUnsignedByte() << 16) | dis.readUnsignedShort());
+			pdb.getPalmRecords().add(palmRecord);
+
+		}
+		return offsets;
+	}
+
 	protected Date readDate(DataInputStream dis) throws IOException {
 		long time = dis.readInt();
 		return new Date(time * 1000 + DATE_OFFSET);
@@ -121,20 +116,9 @@ public class PDBReader {
 		out.println();
 		
 		for(PalmRecord record: pdb.getPalmRecords()) {
-			out.format("Record: id = %4d, attributes = %s, length = %d\n", record.getId(), record.getAttributes(),
+			out.format("Record: id = %4d, attributes = %s, length = %d%n", record.getId(), record.getAttributes(),
 					record.getData() == null ? "null" : record.getData().length);
 		}
 	}
-	
-//	public static void main(String[] args) throws IOException {
-//		File file = new File("test.mobi");
-//
-//		PDBReader reader = new PDBReader();
-//		PalmDataBase pdb = reader.read(file);
-//		reader.dump(System.out, pdb);
-//
-//		MobiAdapter adapter = new MobiAdapter(pdb);
-//		ByteIO.writeFile("test/contents.html", adapter.getTextContents(), Charset.forName("UTF-8"));
-//		ByteIO.writeFile("test/img0.jpg", adapter.getImage(0));
-//	}
+
 }
